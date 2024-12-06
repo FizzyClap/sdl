89b5f24d8b4b0c4392d2e6abb23b373f4f23f59c
Robin Espicier <roespici@c2r11p3.42nice.fr>
2024-12-06 12:23:25 +0100

    compil library

---
+++ SDL2/build/.libs/imKStoUCS.d
@@ -0,0 +1,9 @@
+build/imKStoUCS.lo: \
+ /home/roespici/github/cub3d/sound/SDL2/src/events/imKStoUCS.c \
+ /home/roespici/github/cub3d/sound/SDL2/src/events/../SDL_internal.h \
+ /home/roespici/github/cub3d/sound/SDL2/src/events/../dynapi/SDL_dynapi.h \
+ /home/roespici/github/cub3d/sound/SDL2/src/events/../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+ include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
+ include/SDL_config.h include/SDL_log.h \
+ /home/roespici/github/cub3d/sound/SDL2/src/events/imKStoUCS.h
